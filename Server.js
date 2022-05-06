547962ea203d5234cd15591284b652fa33e98939
Davi Alves <extern.davi.alves@volkswagen.de>
2022-05-06 11:28:44 +0200

    first commit

---
+++ Server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const dataset = require("./DataSet");
+const datasetLarge = require("./DataSetLarge");
+
+const port = 8080;
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send(datasetLarge);
+});
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
