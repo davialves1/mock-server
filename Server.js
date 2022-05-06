9a088863e5f088fda6b9310b84c1bb5e8840d70e
Davi Alves <extern.davi.alves@volkswagen.de>
2022-05-06 11:44:03 +0200

    cors change

--- Server.js
+++ Server.js
@@ -8,6 +8,12 @@ const app = express();
 
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
 app.get("/", (req, res) => {
   res.send(datasetLarge);
 });
