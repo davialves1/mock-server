d6c88be788d2f461a0fee821ea439f8779d8e411
Davi Alves <extern.davi.alves@volkswagen.de>
2022-05-06 15:07:34 +0200

    added docker, added test endpoint

--- Server.js
+++ Server.js
@@ -8,14 +8,18 @@ const app = express();
 
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-})
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// })
 
 app.get("/", (req, res) => {
   res.send(datasetLarge);
 });
+
+app.get("test", (req, res) => {
+  res.send("App Running 🤩")
+})
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
